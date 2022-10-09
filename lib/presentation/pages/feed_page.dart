78bd494eec29fdda82337b0ee6ad182db8fa4e1f
neto <euneto2501@gmail.com>
2022-10-09 03:06:26 -0300

    feature: atualizar layout da tela de feed e do widget do card

--- lib/presentation/pages/feed_page.dart
+++ lib/presentation/pages/feed_page.dart
@@ -8,23 +8,20 @@ class FeedPage extends StatelessWidget {
   final List<Post> posts = [
     Post(
         id: "1",
-        title: "teste descricao",
-        descricao:
-            "teste descricao",
+        title: "Título da oferta",
+        descricao: "Descrição",
         value: 3.0,
         type: true),
     Post(
         id: "2",
-        title: "teste descricao",
-        descricao:
-            "teste descricao",
+        title: "Título da oferta",
+        descricao: "Descrição",
         value: 5.0,
         type: true),
     Post(
         id: "3",
-        title: "teste descricao",
-        descricao:
-            "teste descricao",
+        title: "Título da oferta",
+        descricao: "Descrição",
         value: 6.0,
         type: true),
   ];
@@ -33,37 +30,62 @@ class FeedPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('EasyLanche'),
+        title: const Text(
+          "EasyLanche",
+          style: TextStyle(
+            color: Color.fromARGB(255, 0, 0, 0),
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        titleSpacing: 00.0,
+        centerTitle: true,
+        toolbarHeight: 60.2,
+        toolbarOpacity: 0.8,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(25),
+              bottomLeft: Radius.circular(25)),
+        ),
+        elevation: 0.00,
         backgroundColor: Cores.laranjaPrincipal,
-      ),
+      ), //
       body: Container(
+        constraints: BoxConstraints.expand(),
+        color: Cores.cinzaBackground,
         height: 420,
         child: posts.isEmpty
-            ? Column(
-                children: [
-                  SizedBox(height: 20),
-                  Text(
-                    "Nenhuma Postagem",
-                  ),
-                  SizedBox(height: 20),
-                ],
+            ? Center(
+                child: Column(
+                  children: [
+                    SizedBox(height: 20),
+                    Text(
+                      "Nenhuma Postagem",
+                    ),
+                    SizedBox(height: 20),
+                  ],
+                ),
               )
             : ListView.builder(
                 itemCount: posts.length,
                 itemBuilder: (ctx, index) {
                   final p = posts[index];
-                  return CardOfertaWidget(
-                    title:p.title, 
-                    descricao:p.descricao,
-                    value: p.value
-                    );
+                  return Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: CardOfertaWidget(
+                        title: p.title, descricao: p.descricao, value: p.value),
+                  );
                 },
               ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
+      floatingActionButton: FloatingActionButton.extended(
         backgroundColor: Cores.laranjaPrincipal,
+        foregroundColor: Colors.black,
+        onPressed: () {},
+        label: Text('Criar Oferta',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            )),
+        icon: Icon(Icons.add),
       ),
     );
   }
--- lib/presentation/widgets/feed/card_oferta_widget.dart
+++ lib/presentation/widgets/feed/card_oferta_widget.dart
@@ -16,33 +16,66 @@ class CardOfertaWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: SizedBox(
-        width: 300,
-        height: 100,
-        child: Column(
+    return Container(
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 246, 239, 236),
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: Color.fromARGB(255, 0, 0, 0).withOpacity(0.5),
+            blurRadius: 3,
+            offset: Offset(0, 1),
+          ),
+        ],
+      ),
+      width: double.infinity,
+      child: Container(
+        margin: EdgeInsets.all(8),
+        child: Row(
           children: [
-            Row(
-              children: [
-                Text(
-                  title,
-                  style: TextStyle(
-                    fontSize: 24,
-                    foreground: Paint()
-                      ..style = PaintingStyle.stroke
-                      ..strokeWidth = 2
-                      ..color = Colors.blue[700]!,
-                    fontFamily: 'Open Sans',
-                  ),
-                ),
-              ],
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: Image.asset(
+                'assets/images/salgado.jpg',
+                width: 100,
+                height: 100,
+                fit: BoxFit.contain,
+              ),
             ),
-            Row(children: [
-              Text(
-                descricao,
-                style: TextStyle(fontFamily: 'Open Sans'),
-              )
-            ])
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.only(left: 18),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(title,
+                            style: TextStyle(
+                              fontFamily: 'RobotoMono',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            )),
+                        Icon(Icons.arrow_forward_ios),
+                      ],
+                    ),
+                    Text(descricao,
+                        style: TextStyle(
+                          color: Colors.grey[400],
+                          fontWeight: FontWeight.bold,
+                        )),
+                    Text("R\$ ${value.toString()}0",
+                        style: TextStyle(
+                          height: 2,
+                          fontSize: 15,
+                          color: Colors.green[300],
+                          fontWeight: FontWeight.bold,
+                        )),
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
